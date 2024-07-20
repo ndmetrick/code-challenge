9ec126a3310b08e9398b3b677a167083abbe59d1
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2024-07-20 16:00:22 -0500

    wire up form to send requests to API

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,32 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+/* eslint-disable strict */
+document.addEventListener('DOMContentLoaded', function () {
+  const addressForm = document.querySelector('.form')
+
+  addressForm.addEventListener('submit', parseAddressInput)
+})
+
+const parseAddressInput = async (e) => {
+  e.preventDefault()
+  const input = document.getElementById('address').value
+
+  try {
+    const response = await fetch('/api/parse?input=' + encodeURIComponent(input))
+    if (!response.ok) {
+      throw new Error('Network error')
+    } else {
+      const jsonResponse = await response.json()
+      console.log('what do we have', jsonResponse, 'd', response)
+      const addressComponents = jsonResponse.address_components
+      const addressType = jsonResponse.address_type
+    }
+  } catch (error) {
+    console.log('error', error)
+
+  }
+}
+
+
+
+
+
+/* eslint-disable strict */
