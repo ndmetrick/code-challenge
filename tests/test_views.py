4ca96bd8127345faf8202c8793d2c8a81a7a58de
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2024-07-20 17:50:00 -0500

    add tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,17 @@
-import pytest
+from collections import OrderedDict
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get('/api/parse?input=' + address_string, follow=True)
+    address_components = response.data['address_components']
+    address_type = response.data['address_type']
+    assert response.status_code == 200
+    assert isinstance(address_components, OrderedDict)
+    assert isinstance(address_type, str)
 
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = client.get('/api/parse?input=' + address_string, follow=True)
+    assert response.status_code == 400
